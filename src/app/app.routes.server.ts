f6781014ce1e05a8b6c965fcf89784f17ddb8f26
Samo1999382 <hamadaarafa1999382@gmail.com>
2025-03-07 21:28:51 +0200

    V0.66

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -8,5 +8,9 @@ export const serverRoutes: ServerRoute[] = [
   {
     path: 'productDetails/:id',
     renderMode: RenderMode.Server,
+  },
+  {
+    path: 'checkout/:id',
+    renderMode: RenderMode.Server,
   }
 ];
